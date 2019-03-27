b8dd473f36a06810d1be7730cf0b616383e71e80
Timur Cheberda <likedune@gmail.com>
2019-03-27 22:55:10 +0300

    Initial Commit

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Тимур Чеберда on 27/03/2019.
+//  Copyright © 2019 Tmur Cheberda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
