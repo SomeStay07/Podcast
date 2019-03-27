809383e54f620f9d1b3e4693c05e344e1d3902c2
Timur Cheberda <likedune@gmail.com>
2019-03-27 23:36:24 +0300

    #1.0
    Setup three items

---
+++ Podcast/Controller/MainTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabBarController.swift
+//  Podcast
+//
+//  Created by Тимур Чеберда on 27/03/2019.
+//  Copyright © 2019 Tmur Cheberda. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .purple
+        
+        setupViewController()
+    }
+    
+    //MARK:- Setup functions
+    func setupViewController() {
+        viewControllers = [
+            generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    //MARK:- NavigationController
+    fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+}
