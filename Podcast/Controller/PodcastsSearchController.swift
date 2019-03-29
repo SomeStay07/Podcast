ce5b91e2737317e76d824df5833e122d4be99fa2
Timur Cheberda <likedune@gmail.com>
2019-03-29 12:05:06 +0300

    # 2.0
    + search view, but doesnt work atm
    + model for podcasts

---
+++ Podcast/Controller/PodcastsSearchController.swift
@@ -0,0 +1,52 @@
+//
+//  PodcastsSearchController.swift
+//  Podcast
+//
+//  Created by Тимур Чеберда on 29/03/2019.
+//  Copyright © 2019 Tmur Cheberda. All rights reserved.
+//
+
+import UIKit
+
+class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
+    
+    let podcasts = [ Podcast(name: "12345", artistName: "Denis") ]
+    
+    let cellId = "cellId"
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSearchBar()
+        setupTableView()
+    }
+    //MARK:- UITableView
+    fileprivate func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    fileprivate func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcasts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let podcast = self.podcasts[indexPath.row]
+        cell.textLabel?.text = "\(podcast.name)\n\(podcast.artistName)"
+        cell.textLabel?.numberOfLines = -1
+        cell.imageView?.image = #imageLiteral(resourceName: "appicon")
+        
+        return cell
+    }
+}
---
+++ Podcast/Model/Podcast.swift
@@ -0,0 +1,14 @@
+//
+//  Podcast.swift
+//  Podcast
+//
+//  Created by Тимур Чеберда on 29/03/2019.
+//  Copyright © 2019 Tmur Cheberda. All rights reserved.
+//
+
+
+struct Podcast {
+    let name: String
+    let artistName: String
+    
+}
